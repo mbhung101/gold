6e561ed9c2692e51175b573a45b77d684451aba6
github username <github email address>
2017-12-20 02:07:36 -0500

    test2

--- src/components/voting.js
+++ src/components/voting.js
@@ -26,16 +26,16 @@ export default class Voting extends Component {
               <div className="votingBox2">
                 <div className="votingCheckbox1"></div>
                 <div className="votingCheckmark1">&#x2713;</div>
-                <div className="votingLabel1"> Electing Directors </div>
-                <div className="votingText1"> You will be electing a Board of Directors consisting of 15 members. Please refer to the section entitled “Directors” on page 22 of this Circular for biographies and more information on the nominees. Directors elected at the Meeting will serve until the end of our next annual shareholders’ meeting or until their resignation, if earlier.  </div>
+                <div className="votingLabel1"> Appointing the Auditor </div>
+                <div className="votingText1"> PricewaterhouseCoopers LLP (PwC) has been our external auditor since 1983. The Board, on the recommendation of the Audit Committee, recommends that PwC be reappointed as auditor and that the Board be authorized to set the auditor’s remuneration. The audit firm appointed at the Meeting will serve until the end of the Company’s next annual shareholders’ meeting.</div>
                 <div className="votingRead1">Read More</div>
               </div>
 
               <div className="votingBox3">
                 <div className="votingCheckbox1"></div>
                 <div className="votingCheckmark1">&#x2713;</div>
-                <div className="votingLabel1"> Electing Directors </div>
-                <div className="votingText1"> You will be electing a Board of Directors consisting of 15 members. Please refer to the section entitled “Directors” on page 22 of this Circular for biographies and more information on the nominees. Directors elected at the Meeting will serve until the end of our next annual shareholders’ meeting or until their resignation, if earlier.  </div>
+                <div className="votingLabel1"> Say on Pay Advisory Vote </div>
+                <div className="votingText1"> The Board has adopted a non-binding advisory vote relating to executive compensation to solicit feedback on our approach to executive compensation. The previous say on pay advisory vote held in 2016 was supported with the approval of 90.9% of those shareholders present at our 2016 annual meeting and voting in person or by proxy.  </div>
                 <div className="votingRead1">Read More</div>
               </div>
 
@@ -47,8 +47,8 @@ export default class Voting extends Component {
           <div style={{display:"inlineBlock"}}>
             <div className="votingCast"> Ready to cast your vote? </div>
             <div className="votingButton">Vote Now</div>
-
           </div>
+
         </div>
     )
   }
