a496ec1cf5470aed41c4e54d71178322edb67f85
github username <github email address>
2017-12-19 23:54:45 -0500

    fixed nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -8,17 +8,19 @@ export default class Nav extends Component {
     return(
       <div className="navBar">
         <div className="navBox">
-        <img src={require('../Assets/Barrick_logo.png')} className="logo"/>
+          <img src={require('../Assets/Barrick_logo.png')} className="logo"/>
           <div className="navLinks">
             <a className="navLink"> Our Board</a>
             <a className="navLink"> Our Company</a>
             <a className="navLink"> Our Pay</a>
             <a className="navLink"> 2018 Meeting</a>
           </div>
-          <a className="navProxy"> Download Proxy </a>
-          <a className="navProxy"> Annual Report </a>
-          <div className="navButton"> Vote Now </div>
         </div>
+          <div className="navBox2">
+            <a className="navProxy">Download Proxy </a>
+            <a className="navProxy">Annual Report </a>
+            <div className="navButton">Vote Now</div>
+          </div>
       </div>
     )
   }
