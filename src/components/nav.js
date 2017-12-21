2eb368f705848ade5e38e40bc750047c405c1e4c
github username <github email address>
2017-12-21 15:49:22 -0500

    nav bar reactive

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,23 +4,28 @@ export default class Nav extends Component {
   constructor(){
     super()
   }
+
+
+
   render(){
     return(
       <div className="navBar">
-        <div className="navBox">
+        <div className="navBox1">
           <img src={require('../Assets/Barrick_logo.png')} className="navLogo"/>
-          <div className="navLinks">
-            <a className="navLink"> Our Board</a>
-            <a className="navLink"> Our Company</a>
-            <a className="navLink"> Our Pay</a>
-            <a className="navLink"> 2018 Meeting</a>
-          </div>
         </div>
-          <div className="navBox2">
-            <a className="navProxy">Download Proxy </a>
-            <a className="navProxy">Annual Report </a>
-            <div className="navButton">Vote Now</div>
-          </div>
+        <div className="navBox2">
+          <div className="navLink"> Our Board </div>
+          <div className="navLink"> Our Company </div>
+          <div className="navLink"> Our Pay </div>
+          <div className="navLink"> 2018 Meeting </div>
+        </div>
+        <div className="navBox3">
+          <div className="navProxy"> Download Proxy</div>
+          <div className="navProxy"> Annual Report</div>
+        </div>
+        <div className="navBox4">
+          <div className="navButton">Vote Now</div>
+        </div>
       </div>
     )
   }
--- src/containers/goldcontainer.js
+++ src/containers/goldcontainer.js
@@ -16,20 +16,24 @@ export default class GoldContainer extends Component {
       user: {}
     }
   }
+
+  // <Our/>
+  // <div className="graphHolder">
+  //   <img src={require('../Assets/graph.jpg')} style={{height:"100%",width:"100%"}}/>
+  // </div>
+  // <Chairman/>
+  // <Information/>
+  // <Voting/>
+  // <Annual/>
+  // <Bottom/>
+
+
   render(){
     return(
       <div stlye={{width:"100%",height:"100%"}}>
         <Nav/>
         <Welcome/>
-        <Our/>
-        <div className="graphHolder">
-          <img src={require('../Assets/graph.jpg')} style={{height:"100%",width:"100%"}}/>
-        </div>
-        <Chairman/>
-        <Information/>
-        <Voting/>
-        <Annual/>
-        <Bottom/>
+
       </div>
     )
   }
